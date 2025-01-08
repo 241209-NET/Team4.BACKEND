6c5e0f61394bedc58b6c8a0b88f55336dbf51551
gvamsius <66033004+gvamsius@users.noreply.github.com>
2025-01-08 15:45:51 -0500

    Updated Item

--- ECommerce.API/0_Model/Item.cs
+++ ECommerce.API/0_Model/Item.cs
@@ -9,9 +9,11 @@ public class Item
 
     [ForeignKey("Department")]
     public int DepartmentId { get; set; }
-    public float Price { get; set;}
+    public float Price { get; set; }
     public int Quantity { get; set; }
     public string Name { get; set; } = "";
     public string Description { get; set; } = "";
+
+    public List<Order>? Orders { get; set; } = [];
 
 }
