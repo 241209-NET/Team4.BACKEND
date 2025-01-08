c584c5b8b178effc34b4c362f89ec02d7511d44a
ajpiccolo <ajpiccolo@gmail.com>
2025-01-08 15:24:18 -0500

    SQL Folder

--- ECommerce.API/Migrations/20250108200821_AllDbSets.Designer.cs
+++ ECommerce.API/Migrations/20250108200821_AllDbSets.Designer.cs
@@ -4,6 +4,7 @@ using ECommerce.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ECommerce.API.Migrations
 {
     [DbContext(typeof(ECommerceContext))]
-    partial class ECommerceContextModelSnapshot : ModelSnapshot
+    [Migration("20250108200821_AllDbSets")]
+    partial class AllDbSets
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -29,6 +32,10 @@ namespace ECommerce.API.Migrations
                         .HasColumnType("int");
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                    b.Property<string>("Description")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Name")
                         .IsRequired()
---
+++ ECommerce.API/Migrations/20250108200821_AllDbSets.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerce.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AllDbSets : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Item_Departments_DepartmentId",
+                table: "Item");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Item_Order_OrderId",
+                table: "Item");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Order_Users_UserId",
+                table: "Order");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Order",
+                table: "Order");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Item",
+                table: "Item");
+
+            migrationBuilder.RenameTable(
+                name: "Order",
+                newName: "Orders");
+
+            migrationBuilder.RenameTable(
+                name: "Item",
+                newName: "Items");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Order_UserId",
+                table: "Orders",
+                newName: "IX_Orders_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Item_OrderId",
+                table: "Items",
+                newName: "IX_Items_OrderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Item_DepartmentId",
+                table: "Items",
+                newName: "IX_Items_DepartmentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders",
+                column: "OrderId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Items",
+                table: "Items",
+                column: "ItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Departments_DepartmentId",
+                table: "Items",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Orders_OrderId",
+                table: "Items",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Departments_DepartmentId",
+                table: "Items");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Orders_OrderId",
+                table: "Items");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Items",
+                table: "Items");
+
+            migrationBuilder.RenameTable(
+                name: "Orders",
+                newName: "Order");
+
+            migrationBuilder.RenameTable(
+                name: "Items",
+                newName: "Item");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Orders_UserId",
+                table: "Order",
+                newName: "IX_Order_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Items_OrderId",
+                table: "Item",
+                newName: "IX_Item_OrderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Items_DepartmentId",
+                table: "Item",
+                newName: "IX_Item_DepartmentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Order",
+                table: "Order",
+                column: "OrderId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Item",
+                table: "Item",
+                column: "ItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Item_Departments_DepartmentId",
+                table: "Item",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Item_Order_OrderId",
+                table: "Item",
+                column: "OrderId",
+                principalTable: "Order",
+                principalColumn: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Order_Users_UserId",
+                table: "Order",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
