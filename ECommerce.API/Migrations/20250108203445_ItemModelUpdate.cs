a0e0d2e9408eab776a084657e6c08a11be6ec02a
ajpiccolo <ajpiccolo@gmail.com>
2025-01-08 15:35:21 -0500

    update item model migrations

---
+++ ECommerce.API/Migrations/20250108203445_ItemModelUpdate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerce.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ItemModelUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Price",
+                table: "Items",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+    }
+}
