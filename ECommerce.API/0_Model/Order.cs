c86a6d5a881bc42bbe7fd96ddd4af2f267b41783
Ethan Catania <101900207+ethancatania@users.noreply.github.com>
2025-01-08 14:29:30 -0500

    Created Order model

---
+++ ECommerce.API/0_Model/Order.cs
@@ -0,0 +1,22 @@
+namespace Ecommerce.API.Model;
+
+using ECommerce.API.Model;
+
+public class Order
+{
+    
+    public required int OrderId_PK { get; set; }
+
+    public required int UserId_FK { get; set; }
+
+    public double TotalPrice { get; set; } = 0.0;
+    
+    public List<Item> Items { get; set; } = new List<Item>();
+
+    public bool IsOrdered { get; set; } = false;
+
+    public string Address { get; set; } = "";
+
+    public DateTime OrderDate { get; set; } = DateTime.Now;
+
+}
