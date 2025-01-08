1dd5344d8c0fb5877b15d2d0e36a456c76e6941b
gvamsius <66033004+gvamsius@users.noreply.github.com>
2025-01-08 15:16:24 -0500

    modified Depratment.cs

--- ECommerce.API/0_Model/Department.cs
+++ ECommerce.API/0_Model/Department.cs
@@ -6,7 +6,6 @@ public class Department
 
     public int Id { get; set; }
     public string Name { get; set; } = "";
-    public string Description { get; set; } = "";
 
     public List<Item> Items { get; set; } = [];
 
