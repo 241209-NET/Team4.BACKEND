7eafd1f92dbba6022a2aea08ebe180efae7856e7
ajpiccolo <ajpiccolo@gmail.com>
2025-01-09 23:40:58 -0500

    resolve conflict

--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -1,11 +1,8 @@
 using ECommerce.API.Data;
 using ECommerce.API.Repository;
 using ECommerce.API.Service;
-<<<<<<< HEAD
+
 using Microsoft.EntityFrameworkCore; 
-=======
-using Microsoft.EntityFrameworkCore;
->>>>>>> 692af034f5107ebc812835a0b702cb2eb20547ba
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,22 +16,15 @@ builder.Services.AddDbContext<ECommerceContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ECommerceDB")));
 
 //add service dependencies
-<<<<<<< HEAD
 builder.Services.AddScoped<IUserService, UserService>(); 
 builder.Services.AddScoped<IOrderService, OrderService>(); 
-=======
 builder.Services.AddScoped<IDepartmentService, DepartmentService>();
-
-
-//Dependency Inject the proper repositories
-builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
-
-
->>>>>>> 692af034f5107ebc812835a0b702cb2eb20547ba
 
 //add repo dependencies
 builder.Services.AddScoped<IUserRepository, UserRepository>(); 
 builder.Services.AddScoped<IOrderRepository, OrderRepository>(); 
+builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+
 
 //add controllers
 builder.Services.AddControllers()
