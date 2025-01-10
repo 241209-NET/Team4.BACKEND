d5d8f5168b20e013fb597beff29bdf5872b76260
JudeH1 <90494861+JudeH1@users.noreply.github.com>
2025-01-09 23:07:55 -0600

    Revert "items part 4"
    This reverts commit 0753a14c87305626bfea1766eb6b40885633ebd6.

--- ECommerce.API/3_Repository/Interface/IRepository.cs
+++ ECommerce.API/3_Repository/Interface/IRepository.cs
@@ -1,6 +1,6 @@
-using ECommerce.API.Model; 
+using ECommerce.API.Model;
 
-namespace ECommerce.API.Repository; 
+namespace ECommerce.API.Repository;
 
 public interface IUserRepository
 {
@@ -8,19 +8,15 @@ public interface IUserRepository
     //include the contract for all methods in the UserRepository
     public IEnumerable<User> GetAllUsers();
     public User AddNewUser(User newUser);
-    public User GetUserByName(string userName); 
+    public User GetUserByName(string userName);
     public User GetUserById(int userId);
-    public User UpdateUserById(User updateUser); 
-    public User DeleteUserById(int id); 
+    public User UpdateUserById(User updateUser);
+    public User DeleteUserById(int id);
 
 }
 
 public interface IItemRepository
 {
-    public Item? GetItemById(int id);
-    public IEnumerable<Item>? GetItemsInStock();
-    public Item? AddNewItem(Item item);
-    public Item? DeleteItemById(int id);
 
 }
 
@@ -41,5 +37,15 @@ public interface IOrderRepository
     public DateTime GetOrderStatus(int id);
 
     public Order CompleteCheckout(Order order);
-    
+
+}
+
+public interface IDepartmentRepository
+{
+    public IEnumerable<Department> GetAllDepartments();
+    public Department? GetDepartmentById(int id);
+    public IEnumerable<Department>? GetDepartmentByName(string name);
+    public Department DeleteDepartmentById(int id);
+
+    public Task<Department> AddDepartment(Department department);
 }
