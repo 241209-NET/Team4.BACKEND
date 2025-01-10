0d625ca736cc9b3522285bc6ab77d396ed6af940
JudeH1 <90494861+JudeH1@users.noreply.github.com>
2025-01-09 23:01:29 -0600

    forgot to save controller changes whoops
    but here they are

---
+++ ECommerce.API/1_Controllers/ItemController.cs
@@ -0,0 +1,45 @@
+using ECommerce.API.Model; 
+using ECommerce.API.Service; 
+using Microsoft.AspNetCore.Mvc; 
+
+namespace ECommerce.API.Controller; 
+
+[Route("api/[controller]")]
+[ApiController]
+public class ItemController : ControllerBase
+{
+    private readonly IItemService _itemService;
+
+    public ItemController(IItemService itemService)
+    {
+        _itemService = itemService;
+    }
+
+    //list an item by id
+    [HttpGet("{id}")]
+    public IActionResult GetItemById(int id)
+    {
+        return Ok(_itemService.GetItemById(id));
+    }
+
+    // get items in stock
+    [HttpGet]
+    public IActionResult GetItemsInStock()
+    {
+        return Ok(_itemService.GetItemsInStock());
+    }
+
+    // add item
+    [HttpPost]
+    public IActionResult AddItem(Item newItem)
+    {
+        return Ok(_itemService.AddNewItem(newItem));
+    }
+    // delete item by id
+    [HttpDelete("{id}")]
+    public IActionResult DeleteItemById(int id)
+    {
+        return Ok(_itemService.DeleteItemById(id));
+    }
+
+}
